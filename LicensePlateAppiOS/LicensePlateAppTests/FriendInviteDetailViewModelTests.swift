dbc0cfa877143c2e816ef0691ce3ef54eeb38e4b
Chris Hammers <83467020+ChrisHammers@users.noreply.github.com>
2026-03-26 09:57:16 -0400

    Step 10 - Test fix.

--- LicensePlateAppiOS/LicensePlateAppTests/FriendInviteDetailViewModelTests.swift
+++ LicensePlateAppiOS/LicensePlateAppTests/FriendInviteDetailViewModelTests.swift
@@ -10,9 +10,10 @@ final class FriendInviteDetailViewModelTests: XCTestCase {
 
     @MainActor
     func testStoresInviteId() {
-        let vm = FriendInviteDetailViewModel(inviteId: "invite-123")
-        XCTAssertEqual(vm.inviteId, "invite-123")
-        XCTAssertFalse(vm.hasAccepted)
-        XCTAssertNil(vm.user)
+        var vm: FriendInviteDetailViewModel? = FriendInviteDetailViewModel(inviteId: "invite-123")
+        XCTAssertEqual(vm?.inviteId, "invite-123")
+        XCTAssertFalse(vm?.hasAccepted ?? true)
+        XCTAssertNil(vm?.user)
+        vm = nil
     }
 }
