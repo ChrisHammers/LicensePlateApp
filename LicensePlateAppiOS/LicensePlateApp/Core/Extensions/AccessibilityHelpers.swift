92308e4e031ddd56c55cc3bde947e70c18a04eb1
Chris Hammers <83467020+ChrisHammers@users.noreply.github.com>
2026-06-24 15:44:16 -0400

    Interesting modifier I learned.  not used currently.

--- LicensePlateAppiOS/LicensePlateApp/Core/Extensions/AccessibilityHelpers.swift
+++ LicensePlateAppiOS/LicensePlateApp/Core/Extensions/AccessibilityHelpers.swift
@@ -169,3 +169,13 @@ func withAccessibleAnimation<T>(
     }
 }
 
+extension View {
+    @ViewBuilder
+    func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+}
